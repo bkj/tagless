f3f8070a466bbb1634e681422600b86482e6a60f
bkj <bkj.322@gmail.com>
2017-10-02 20:57:05 -0400

    adding run-elasticsearch.sh

---
+++ utils/run-elasticsearch.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# run-elasticsearch.sh
+#
+# Rough example of how to use the Elasticsearch version
+
+python -m tagless \
+    --sampler elasticsearch \
+    --filenames fnames \
+    --img-dir /srv/e2/instagram/trickle/images/ \
+    --labels 'lax,nyc'
