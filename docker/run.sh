5ea24c2e43c5d16b83e365652c76a4dab408f8bf
bjohnson <ben@gophronesis.com>
2017-10-02 22:28:35 -0400

    ...

--- docker/run.sh
+++ docker/run.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 
 IMG_DIR="/srv/e2/instagram/trickle/images/"
+FILENAMES="fnames"
 
 sudo docker run -it \
     -p 5000:5000 \
-    -v $IMG_DIR:/images/ \
-    -v fnames:/fnames \
+    -v $IMG_DIR:/img_dir/ \
+    -v $FILENAMES:/filenames \
     tagless python -m tagless \
         --sampler elasticsearch \
-        --filenames /fnames \
-        --img-dir /images/ \
+        --filenames /filenames \
+        --img-dir /img_dir/ \
         --labels 'LA,NY'
