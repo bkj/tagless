5182d15a56dafba9ab58b5d7fbae35d85d8f2cf3
jgawrilo <justin.gawrilow@gmail.com>
2018-07-05 20:46:53 -0400

    with or without seeds

--- run_twitter.sh
+++ run_twitter.sh
@@ -1,1 +1,5 @@
-python -m tagless --outpath ./target_dir/results/my-twitter-labels --inpath ./target_dir/data/twitter --img-dir "" --n-return 1
+# WITHOUT SEEDS
+# python -m tagless --outpath ./target_dir/results/my-twitter-labels --inpath ./target_dir/data/twitter --img-dir "" --n-return 1
+
+# WITH SEEDS
+python -m tagless --outpath ./target_dir/results/my-twitter-labels --inpath ./target_dir/data/twitter --img-dir "" --n-return 1 --seeds ./target_dir/data/twitter_seeds
